da445d87fdb8a989bd9dc84d7263545d14a5ea3f
Fabrice Scherer <fabrice.eweb@gmail.com>
2019-06-03 17:07:23 +0200

    starting

---
+++ app/controllers/bundles_controller.rb
@@ -0,0 +1,34 @@
+class BundlesController < ApplicationController
+  def index
+    @bundles = Bundle.all
+  end
+
+  def new
+    @bundle = Bundle.new
+  end
+
+  def create
+    @bundle = Bundle.new(bundle_params)
+    if @bundle.save
+      redirect_to bundle_path(@bundle)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def bundle_params
+    params.require(:bundle).permit(:product_1, :product_2)
+  end
+
+end
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,5 @@
+class ProductsController < ApplicationController
+  def index
+    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+  end
+end
