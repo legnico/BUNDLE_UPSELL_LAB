b8dfc7cff73bf3ba09bb17fe7bc35091f914de56
Nicolas Leguillon <legnico75@gmail.com>
2019-06-03 17:25:51 +0200

    first commit shopify project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
+
 Rails.application.routes.draw do
-  root :to => 'home#index'
+  root to: 'home#index'
   mount ShopifyApp::Engine, at: '/'
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
