ded85493ff6bfb1274b41fe3e96ef1941fa227c7
Nicolas Leguillon <legnico75@gmail.com>
2019-06-03 18:29:29 +0200

    update home page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,5 @@
 class ProductsController < ApplicationController
-<<<<<<< HEAD
-=======
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
->>>>>>> bee99d63a5e8b12462b7e3381910e47640be607f
 end
