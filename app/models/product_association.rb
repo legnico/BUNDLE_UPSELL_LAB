2bf6c239d166583f10163482e0c981fe472de181
Fabrice Scherer <fabrice.eweb@gmail.com>
2019-06-05 18:16:03 +0200

    api almost finished

--- app/models/product_association.rb
+++ app/models/product_association.rb
@@ -1,2 +1,5 @@
 class ProductAssociation < ApplicationRecord
+  def self.find_associated_products(shopify_product_id)
+    ProductAssociation.where(primary_product_id: shopify_product_id.to_s, published: true).map { |x| x["associated_product_id"] }
+  end
 end
