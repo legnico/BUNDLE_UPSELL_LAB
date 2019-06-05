18f67a937101045626afb446d82595452d88ce0e
Thomas CHOPITEL <thomas.chopitel@yahoo.fr>
2019-06-05 16:53:12 +0200

    merge

--- app/models/product_association.rb
+++ app/models/product_association.rb
@@ -1,2 +1,5 @@
 class ProductAssociation < ApplicationRecord
+  def self.find_associated_products(shopify_id)
+    ProductAssociation.where(primary_product_id: shopify_id.to_s).map { |x| x["associated_product_id"] }
+  end
 end
